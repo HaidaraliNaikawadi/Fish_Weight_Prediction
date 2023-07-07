a271f850e138bedcc8630c4a5e16678ef090e951
HaidaraliNaikawadi <138418238+HaidaraliNaikawadi@users.noreply.github.com>
2023-07-07 13:14:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,6 @@ import os
 
 PORT_NUMBER=8080
 
-#path1='static\Model'
-#path2='static\Data\json_file'
 
-model_file_path=os.path.join('static','Model','linear_regression_fish.pkl','')
-json_file_path=os.path.join('static','Data','json_file','json_data_fish.json','')
+model_file_path='linear_regression_fish.pkl'
+json_file_path='json_data_fish.json'
